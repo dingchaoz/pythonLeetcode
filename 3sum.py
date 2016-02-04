6777dcde8fdb0fa490e0394f576e018f776c2679
Dingchao Zhang <dingchaoie@gmail.com>
2016-02-03 20:22:06 -0500

    add comment

--- 3sum.py
+++ 3sum.py
@@ -6,7 +6,7 @@ Find all unique triplets in the array which gives sum of zero
 Note:
 Elements in a triplet(a,b,c) must be in non-descending order(ie,a<=b<=c)
 The solution set must not contain duplicate triplets,
-    For example, given array S = [-1, 0, 1, 2, -1, -4]
+    For example, given array S = [-1, 0, 1, 2, -1, -4];s
 
     A solution set is:
         (-1,0,1)
